f9e76b13f5bbca413b84186317292dbf02e6ab11
Chrispo <christiancastillo0350@gmail.com>
2025-09-17 23:17:11 -0700

    Avance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import queue
+import random
 
 class Estadisticas:
     def __init__(self, 
@@ -12,40 +13,54 @@ class Estadisticas:
         self.sum_time_wait = sum_time_wait
         self.time_open_cashier = time_open_cashier
 
-class GenerardoLlegadas:
-    def __init__(self,
-                 tiempo_medio: float,
-                 proxima_llegada: float,
-                 contador_clientes: int
-                ):
-        self.tiempo_medio = tiempo_medio
-        self.proxima_llegada = proxima_llegada
-        self.contador_clientes = contador_clientes
-
-
 class Cliente:
     def __init__(self,
                  id: int,
                  tiempo_llegada: float,
-                 tiempo_inicio_atencion: float,
-                 tiempo_fin_atencion: float,
+                 tiempo_inicio_atencion = 0,
+                 tiempo_fin_atencion = 0,
                 ):
         self.id = id
         self.tiempo_llegada = tiempo_llegada
         self.tiempo_inicio_atencion = tiempo_inicio_atencion
         self.tiempo_fin_atencion = tiempo_fin_atencion
 
+class GenerardoLlegadas:
+    def __init__(self,
+                 tiempo_medio: float,
+                 proxima_llegada = 0,
+                 contador_clientes = 0
+                ):
+        self.tiempo_medio = tiempo_medio
+        self.proxima_llegada = proxima_llegada
+        self.contador_clientes = contador_clientes
+        
+    def generarLlegada(self) -> float:
+        pass
+    
+    def esTiempoDeLlegada(self, tiempo_actual: float):
+         pass
+    def crearCliente(self) -> Cliente:
+         pass
+
 class FilaClientes:
     def __init__(self,
-                 cola: queue,
                  len_max: int,
                  len_sum: float,
-                 mediciones: int
+                 mediciones: int,
+                 contador_id = 0,
+                 cola = queue.SimpleQueue()
                 ):
         self.cola = cola
         self.len_max = len_max
         self.len_sum = len_sum
         self.mediciones = mediciones
+        self.contador_id = contador_id
+    
+    def agregarCliente(self, tiempo_llegada):
+         self.contador_id += 1
+         new_cliente = Cliente(self.contador_id, tiempo_llegada)
+         self.cola.put(new_cliente)
 
 class Caja:
     def __init__(self,
@@ -85,16 +100,21 @@ class Simulador():
         self.estadisticas = estadisticas
     
     def iniciarSimulacion(self):
-        fila_de_clientes = Cliente(queue.SimpleQueue(), )
+        fila_de_clientes = FilaClientes(100, 0, 0)
         
+        '''
+        random.expovariate(1.0): Genera números aleatorios con distribución exponencial. El parámetro 1.0 significa que la media es 1 minuto.
+        '''
+        proxima_llegada = random.expovariate(1.0)
+
         while self.tiempo_actual < self.tiempo_simulacion:
                 # print(self.tiempo_actual)
                 if self.tiempo_actual % 1 == 0 and self.tiempo_actual > 0:
                         print(self.tiempo_actual)
-                
-                self.tiempo_actual += 0.5
+                        fila_de_clientes.agregarCliente(self.tiempo_actual)
+                self.tiempo_actual = round(self.tiempo_actual + 0.1, 1)
 
-
+        print(fila_de_clientes.cola.qsize())
 
 def main():
     simulacion = Simulador(7) # 7 horas
